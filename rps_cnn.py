fe77c546c42fad14f87b5fb1af601e3f3bdb8ace
Kragon Space <kragonj@gmail.com>
2024-02-27 19:24:42 -0800

    preprocess and label the dataset

--- rps_cnn.py
+++ rps_cnn.py
@@ -35,3 +35,78 @@ NUM_CLASSES = dataset_info.features['label'].num_classes
 print('Number of training examples:', NUM_TRAIN_EXAMPLES)
 print('Number of test examples:', NUM_TEST_EXAMPLES)
 print('Number of labeled classes:', NUM_CLASSES)
+
+# step1 - Preprocessing the image - 1: resize and reshape
+INPUT_IMG_SIZE_ORIGINAL = dataset_info.features['image'].shape[0]
+INPUT_IMG_SHAPE_ORIGINAL = dataset_info.features['image'].shape
+
+INPUT_IMG_SIZE_REDUCED = INPUT_IMG_SIZE_ORIGINAL // 2
+INPUT_IMG_SHAPE_REDUCED = (
+    INPUT_IMG_SIZE_REDUCED,
+    INPUT_IMG_SIZE_REDUCED,
+    INPUT_IMG_SHAPE_ORIGINAL[2]
+)
+
+# Here we may switch between bigger or smaller image sized that we will train our model on.
+INPUT_IMG_SIZE = INPUT_IMG_SIZE_REDUCED
+INPUT_IMG_SHAPE = INPUT_IMG_SHAPE_REDUCED
+
+print('Input image size (original):', INPUT_IMG_SIZE_ORIGINAL)
+print('Input image shape (original):', INPUT_IMG_SHAPE_ORIGINAL)
+print('\n')
+print('Input image size (reduced):', INPUT_IMG_SIZE_REDUCED)
+print('Input image shape (reduced):', INPUT_IMG_SHAPE_REDUCED)
+print('\n')
+print('Input image size:', INPUT_IMG_SIZE)
+print('Input image shape:', INPUT_IMG_SHAPE)
+
+# step2 - Function to convert label ID to labels string.
+get_label_name = dataset_info.features['label'].int2str
+
+print(get_label_name(0));
+print(get_label_name(1));
+print(get_label_name(2));
+
+# Data Visualization
+def preview_dataset(dataset):
+    plt.figure(figsize=(12, 12))
+    plot_index = 0
+    for features in dataset.take(12):
+        (image, label) = features
+        plot_index += 1
+        plt.subplot(3, 4, plot_index)
+        # plt.axis('Off')
+        label = get_label_name(label.numpy())
+        plt.title('Label: %s' % label)
+        plt.imshow(image.numpy())
+
+# Explore raw training dataset images.
+preview_dataset(dataset_train_raw)
+# Explore what values are used to represent the image. 
+(first_image, first_lable) = list(dataset_train_raw.take(1))[0]
+print('Label:', first_lable.numpy(), '\n')
+print('Image shape:', first_image.numpy().shape, '\n')
+print(first_image.numpy())
+
+# Step 1(cont.) - Preprocess - convert color and resize the image
+def format_example(image, label):
+    # Make image color values to be float.
+    image = tf.cast(image, tf.float32)
+    # Make image color values to be in [0..1] range.
+    image = image / 255.
+    # Make sure that image has a right size
+    image = tf.image.resize(image, [INPUT_IMG_SIZE, INPUT_IMG_SIZE])
+    return image, label
+
+dataset_train = dataset_train_raw.map(format_example)
+dataset_test = dataset_test_raw.map(format_example)
+
+# Explore what values are used to represent the image. 
+(first_image, first_lable) = list(dataset_train.take(1))[0]
+print('Label:', first_lable.numpy(), '\n')
+print('Image shape:', first_image.numpy().shape, '\n')
+print(first_image.numpy())
+
+# Explore preprocessed training dataset images.
+preview_dataset(dataset_train)
+
